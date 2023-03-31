7b686459581b204d5a59ab6993e39119165d4b30
Pavanitha09 <83276635+Pavanitha09@users.noreply.github.com>
2023-03-31 12:29:30 +0530

    Update extracting_features.py

--- extracting_features.py
+++ extracting_features.py
@@ -17,7 +17,7 @@ FORMAT : STRING
 3) number of digits in the audio file
 FORMAT : STRING
 
-OUTPUT : creates a directory new directory for storing vectors where the directory structure is of the form : "{folder_name}/{speaker_number}/{session}/{number_of_digits}"
+OUTPUT : creates a directory new directory for storing vectors where the directory structure is of the form : "{folder_name}/{speaker_number}/{session}/{number_of_digits}" 
 
 (Example: data_vectors/001/A/2/50.npy)
 
@@ -26,33 +26,33 @@ OUTPUT : creates a directory new directory for storing vectors where the directo
 def extracting_features(speaker_path , vector_path, number_of_digits):
 
     # Creating a folder for each speaker in the vectors folder
-
+    
     vectors_speaker_path = vector_path + '/' + re.split(r'/|\|//|\\', speaker_path)[-1] + '/'
     print(vectors_speaker_path)
     try:
         os.makedirs(vectors_speaker_path)
     except:
         pass
-    speaker_sessions = glob(speaker_path+"*/")
+    speaker_sessions = glob(speaker_path+"/*/")
     speaker_sessions.sort()
     for session in speaker_sessions:
 
         # Creating a folder for each session in vectors folder
 
-        vector_session = vectors_speaker_path + re.split(r'/|\|//|\\', session)[-1] + '/'
+        vector_session = vectors_speaker_path + re.split(r'/|\|//|\\', session)[-2] + '/'
         try:
             os.makedirs(vector_session)
         except:
             pass
 
         # Creating the digits folder in each session
-
+    
         digits_folder = vector_session + str(number_of_digits) + '/'
         try:
             os.makedirs(digits_folder)
         except:
             pass
-
+        
         audios_files_combinations = glob(session + 'merged_audios/' + str(number_of_digits) + '/*.wav')
         audios_files_combinations.sort()
         for combination in audios_files_combinations:
@@ -67,17 +67,17 @@ def extracting_features(speaker_path , vector_path, number_of_digits):
 
 if __name__ == '__main__':
 
-    # Main function generates numpy files for all the audio files in the data folder
-    #---------------------------------------------------------------------
+# Main function generates numpy files for all the audio files in the data folder
+#---------------------------------------------------------------------
     number_of_digits = input("number of digits : ")
-    speakers = glob("./data/*/")
+    speakers = glob("./data/*")
     speakers.sort()
 
     # To create a folder just to store numpy files if it doesnot exist
 
     vector_path = os.getcwd() + '/data_vectors'
     try:
-        os.makedirs(vector_path)
+        os.makedirs(vector_path)      
     except OSError:
         pass
     print(vector_path)
