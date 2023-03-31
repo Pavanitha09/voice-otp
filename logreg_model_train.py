c85369336ece1ffc375ebe5634f5e877292be9ea
McQueen <63579517+mcqueen444@users.noreply.github.com>
2023-03-31 23:24:21 +0530

    Update logreg_model_train.py

--- logreg_model_train.py
+++ logreg_model_train.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import time
 import pickle
+import re
 
 
 # train_model function trains a LogisticRegression model with given train data and train labels, saves model as .pkl file with given model_name
@@ -27,7 +28,7 @@ def train_model(train_data, train_labels, model_name):
 
 
 if __name__ == '__main__':
-    folder_path = "./data/" # path to data folder
+    folder_path = "./data_vectors/" # path to data folder
     folders = glob(folder_path+"/[0-9]*/") # returns list of path to all speaker folders ii data folder
     folders.sort()
 
@@ -40,13 +41,18 @@ if __name__ == '__main__':
     for speaker_path in folders:
         l = glob(speaker_path+"/*['A','B','C']/"+str(n)+'/*.npy')
         l.sort()
-        label = re.findall(r"[\w']+", speaker_path)[-4]
+        label = re.split(r'/|\|//|\\', speaker_path)[-2]
+        print(label)
         for vector_path in l:
-            file = np.load(str(vector))
-            train_data.append(file)
+            file = np.load(str(vector_path))
+            train_data.append(file[0])
             train_labels.append(label)
 
-    model_name = "logreg"+n+".pkl"
+    try:
+        os.makedirs("models")
+    except:
+        pass
+    model_name = "./models/logreg"+n+".pkl"
     model, train_time = train_model(train_data, train_labels, model_name)
 
     print("Model training time :", train_time)
